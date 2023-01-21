ea3c32297f4efb3251aef59b5e2b13b85eba3cdd
Maciej Okninski <okninski.mac@gmail.com>
2023-01-21 21:08:08 +0100

    feat: add mf component to app, poc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
+const Container = React.lazy(() => import("ui/Container"));
 
 const App = () => {
-  return <div>Hello from the app</div>;
+  return (
+    <div>
+      <div>Hello from the App</div>
+      <Suspense fallback={<div>Loading remote...</div>}>
+        <Container />
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
