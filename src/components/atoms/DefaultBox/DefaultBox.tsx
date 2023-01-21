8891e15902ee2d98501631503a70c2755be6fdca
Maciej Okninski <okninski.mac@gmail.com>
2023-01-21 21:08:13 +0100

    feat: add select dropdown component

---
+++ src/components/atoms/DefaultBox/DefaultBox.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import * as colors from "../../../constants/colors";
+
+type Props = {
+  hasFocus?: boolean;
+};
+
+const DefaultBox = styled.div<Props>`
+  width: 100%;
+  height: 100%;
+  background: transparent;
+  color: ${colors.BLACK};
+  outline: none;
+
+  border: 1px solid ${colors.SHADOW};
+  padding: 6px 12px;
+
+  &:focus-within {
+    border-color: ${colors.BLACK};
+  }
+  ${({ hasFocus }) => hasFocus && `border-color: ${colors.BLACK};`}
+`;
+
+export default DefaultBox;
