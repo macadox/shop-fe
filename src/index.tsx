07af25f3eecaba20e2aba3049d0dbd16f6099ea7
Maciej Okninski <59931982+macadox@users.noreply.github.com>
2023-02-16 18:54:29 +0100

    disable strictmode in other env than prod

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,25 @@ import "./services/i18n/i18n";
 
 import { createRoot } from "react-dom/client";
 
+const isDev = process.env.NODE_ENV === "development";
 const container = document.getElementById("app-root")!;
 const root = createRoot(container);
 
-root.render(
-  <React.StrictMode>
+const AppComponents = () => {
+  return (
     <React.Suspense fallback={<div />}>
       <GlobalStyles />
       <App />
     </React.Suspense>
-  </React.StrictMode>
+  );
+};
+
+root.render(
+  isDev ? (
+    <React.StrictMode>
+      <AppComponents />
+    </React.StrictMode>
+  ) : (
+    <AppComponents />
+  )
 );
